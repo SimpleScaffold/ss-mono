d31c7b3329b1daaaecd3d196ba0c1972f0f18e49
김해준 <ravenkim97@onpoom.co.kr>
2026-01-20 10:05:38 +0900

    ㅊㅊ

--- apps/fe/vite1/src/Vite1.tsx
+++ apps/fe/vite1/src/Vite1.tsx
@@ -1,5 +1,7 @@
 import { Button } from '@repo/fe-ui/button';
 import { Card } from '@repo/fe-ui/card';
+import { Dialog, DialogTrigger } from '@repo/fe-ui/dialog';
+import { DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogFooter, DialogClose } from '@repo/fe-ui/dialog';
 import './styles.css';
 
 function Vite1() {
@@ -25,6 +27,38 @@ function Vite1() {
             <Button>Action</Button>
           </div>
         </div>
+
+
+        <Dialog>
+      <form>
+        <DialogTrigger asChild>
+          <Button >Open Dialog</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Edit profile</DialogTitle>
+            <DialogDescription>
+              Make changes to your profile here. Click save when you&apos;re
+              done.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4">
+            <div className="grid gap-3">
+ㅁㅁㅁㅁㅁㅁㅁㅁ
+            </div>
+            <div className="grid gap-3">
+    ㅁㅁㅁㅁㅁㅁ
+            </div>
+          </div>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DialogClose>
+            <Button type="submit">Save changes</Button>
+          </DialogFooter>
+        </DialogContent>
+      </form>
+    </Dialog>
       </div>
     </div>
   );
