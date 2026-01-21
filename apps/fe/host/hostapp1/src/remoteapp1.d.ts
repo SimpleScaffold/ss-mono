725275f4bea514a921f7d699708cb7887016a106
김해준 <ravenkim97@onpoom.co.kr>
2026-01-21 13:52:21 +0900

    타입 지정

---
+++ apps/fe/host/hostapp1/src/remoteapp1.d.ts
@@ -0,0 +1,7 @@
+// Module Federation Remote Module 타입 선언
+declare module 'remoteapp1/RemoteApp1' {
+  import { ComponentType } from 'react';
+  
+  const RemoteApp1: ComponentType;
+  export default RemoteApp1;
+}
