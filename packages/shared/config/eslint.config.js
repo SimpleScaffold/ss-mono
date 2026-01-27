8a2a86900dca7176e06167684bb96ef32a0c4792
김해준 <ravenkim97@onpoom.co.kr>
2026-01-27 16:08:32 +0900

    lint 수정

--- packages/shared/config/eslint.config.js
+++ packages/shared/config/eslint.config.js
@@ -1,11 +1,23 @@
 import js from '@eslint/js'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
+import tseslint from 'typescript-eslint'
 
 export default [
     js.configs.recommended,
+    ...tseslint.configs.recommended,
     {
         files: ['**/*.{js,jsx,ts,tsx}'],
+        languageOptions: {
+            parser: tseslint.parser,
+            parserOptions: {
+                ecmaVersion: 'latest',
+                sourceType: 'module',
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+        },
         plugins: {
             react,
             'react-hooks': reactHooks,
@@ -14,6 +26,8 @@ export default [
             ...react.configs.recommended.rules,
             ...reactHooks.configs.recommended.rules,
             'react/react-in-jsx-scope': 'off',
+            '@typescript-eslint/no-unused-vars': 'warn',
+            'no-unused-vars': 'off', // TypeScript 버전 사용
         },
         settings: {
             react: {
